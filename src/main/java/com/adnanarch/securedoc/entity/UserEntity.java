8e9d7f3b212fd81e43009ccad490e57e91aa6fb9
AdnanArch <thedynamiccoder@gmail.com>
2024-04-06 01:36:49 +0500

    mapping done

---
+++ src/main/java/com/adnanarch/securedoc/entity/UserEntity.java
@@ -0,0 +1,58 @@
+package com.adnanarch.securedoc.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+import static jakarta.persistence.FetchType.EAGER;
+
+/**
+ * Author: Adnan Rafique
+ * Date: 4/6/2024
+ * Time: 12:52 AM
+ * Version: 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@JsonInclude(NON_DEFAULT)
+public class UserEntity extends Auditable{
+    @Column(unique = true, updatable = false, nullable = false)
+    private String userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    private Integer loginAttempts;
+    private LocalDateTime lastLogin;
+    private String phone;
+    private String bio;
+    private String imageUrl;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean enabled;
+    private boolean mfa;
+    @JsonIgnore
+    private boolean qrCodeSecret;
+    @Column(columnDefinition = "TEXT")
+    private String qrCodeImageUri;
+    @ManyToOne(fetch = EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private RoleEntity role;
+
+}
